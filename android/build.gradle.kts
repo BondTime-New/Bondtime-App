ba33a5ac7e10042adbed2409d1d37be56bd6568c
Sithija Lakshan Karunasena <sithikaru@gmail.com>
2025-02-26 12:45:13 +0530

    feat: Integrate Firebase services for core functionality, authentication, and storage

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -1,3 +1,14 @@
+// Top-level build file where you can add configuration options common to all sub-projects/modules.
+buildscript {
+    dependencies {
+        classpath("com.google.gms:google-services:4.3.15")
+    }
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
 allprojects {
     repositories {
         google()
@@ -12,6 +23,7 @@ subprojects {
     val newSubprojectBuildDir: Directory = newBuildDir.dir(project.name)
     project.layout.buildDirectory.value(newSubprojectBuildDir)
 }
+
 subprojects {
     project.evaluationDependsOn(":app")
 }
